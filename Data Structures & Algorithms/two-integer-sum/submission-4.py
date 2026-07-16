f6ad5dd33b1469a36d05266d04288371ffbc0bef
vaishnavi-nibh <vaishnavi.nibhanupudi@gmail.com>
2026-07-16 15:43:44 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #seen maintains all the elements we've seen so far IN THE SAME ORDER TOO (lists preserve order)
+        seen = {}
+
+        for index, num in enumerate(nums):
+            #this means an element we've explored already plus the current number = target
+            if target - num in seen:
+                return [seen[target - num], index] #note, we use a hashmap for O(1) lookup
+                
+            seen[num] = index
+            
+            
+        
